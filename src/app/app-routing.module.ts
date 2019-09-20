7c490ee485881dce50b4aad0913aa46fbe710dee
saeedehd <saeedeh.deldar@gmail.com>
2019-09-20 10:34:08 +0430

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticateComponent } from '../app/authenticate/authenticate.component';
+import { EditPageComponent } from '../app/edit-page/edit-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'authentication' , component: AuthenticateComponent},
+  { path: 'edit' , component: EditPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
